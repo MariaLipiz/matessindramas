eaa95483ece930be95349b97ccfdbee57fc80719
MariaLipiz <marialipizfernandez@gmail.com>
2025-07-01 16:12:44 +0200

    Cambios en email

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -34,11 +34,12 @@ export async function POST(req: Request) {
     console.log('✅ Contacto añadido a Resend:', contact)
 
     // 2. Enviar email de bienvenida
-  const welcomeEmail = await resend.emails.send({
+const welcomeEmail = await resend.emails.send({
   from: FROM_EMAIL,
   to: email,
   subject: 'Aquí no hay truco',
   html: `
+  <div style="font-size: 1.125rem;">
     <p>Te doy la bienvenida y te cuento algo.</p>
 
     <p>Después de 10 años impartiendo clases particulares, vendiendo cursos, trabajando con academias grandes y pequeñas y ayudando 
@@ -49,23 +50,25 @@ export async function POST(req: Request) {
     <p><strong>1- El mundo de la enseñanza está lleno de gente que no sabe enseñar.</strong></p>
     <p>Me explico:</p>
     <p>NO vendo cursos donde te digo "sigue esta fórmula para resolver X y aprueba Y a la primera" ni enseño a mis alumnos tonterías como esas.</p>
-    <p>Primero: porque estaría inculcando todo lo contrario a lo que se necesita para superar cualquier asignatura de matemáticas con éxito.</p>
-    <p>Segundo: porque te estaría engañando. Aquí no hay receta mágica que te salve.</p>
+    <p>Primero: porque estaría inculcando todo lo contrario a lo que se necesita para superar cualquier asignatura de matemáticas.</p>
+    <p>Segundo: porque te estaría engañando. Aquí no hay receta mágica.</p>
 
-    <p>Soy matemática, enseño la que probablemente es la rama de conocimiento más odiada y amada a partes iguales, y lo hago como lo haría un matemático: de forma lógica, estructurada y sin enseñar a memorizar.</p>
+    <p>Soy matemática, enseño la rama de conocimiento más odiada de todas y lo hago como 
+    lo haría un matemático: de forma lógica, entretenida, estructurada y sin enseñar a memorizar.</p>
 
     <p>Si aprendes de esta forma, no habrá profesor ni examen que te quite tu aprobado.</p>
 
     <p><strong>2- Con todo el ruido que hay ahí fuera, es muy difícil tomar buenas decisiones.</strong></p>
 
     <p>Todo lo que enseño y la manera en la que lo enseño es fruto de mi experiencia.</p>
-    <p>A enseñar se aprende enseñando, y a aprobar se aprende aprobando.</p>
+    
     <p>No vale de nada replicar lo que otros hacen si no estás seguro de que funciona.</p>
 
     <p>Así que te mandaré un email semanal con un consejo para aprobar más y aprender mejor.</p>
     <p>Aquí el primero: las definiciones son tus mejores amigas.</p>
 
     <p>Pasa un buen día</p>
+    </div>
   `,
   text: `
 Te doy la bienvenida y te cuento algo.
@@ -78,24 +81,19 @@ He observado dos tendencias que requieren atención urgente:
 1- El mundo de la enseñanza está lleno de gente que no sabe enseñar.
 
 Me explico:
-
 NO vendo cursos donde te digo "sigue esta fórmula para resolver X y aprueba Y a la primera" ni enseño a mis alumnos tonterías como esas.
 
-Primero: porque estaría inculcando todo lo contrario a lo que se necesita para superar cualquier asignatura de matemáticas con éxito.
+Primero: porque estaría inculcando todo lo contrario a lo que se necesita para superar cualquier asignatura de matemáticas.
+Segundo: porque te estaría engañando. Aquí no hay receta mágica.
 
-Segundo: porque te estaría engañando. Aquí no hay receta mágica que te salve.
-
-Soy matemática, enseño la que probablemente es la rama de conocimiento más odiada y amada a partes iguales, y lo hago como lo haría un matemático: de forma lógica, estructurada y sin enseñar a memorizar.
+Soy matemática, enseño la rama de conocimiento más odiada de todas y lo hago como lo haría un matemático: de forma lógica, entretenida, estructurada y sin enseñar a memorizar.
 
 Si aprendes de esta forma, no habrá profesor ni examen que te quite tu aprobado.
 
 2- Con todo el ruido que hay ahí fuera, es muy difícil tomar buenas decisiones.
 
 Todo lo que enseño y la manera en la que lo enseño es fruto de mi experiencia.
-
-A enseñar se aprende enseñando, y a aprobar se aprende aprobando.
-
-No vale de nada replicar lo que otros hacen.
+No vale de nada replicar lo que otros hacen si no estás seguro de que funciona.
 
 Así que te mandaré un email semanal con un consejo para aprobar más y aprender mejor.
 
@@ -104,6 +102,7 @@ Aquí el primero: las definiciones son tus mejores amigas.
 Pasa un buen día
 `
 })
+
 
 
     console.log('📧 Email de bienvenida enviado:', welcomeEmail)
