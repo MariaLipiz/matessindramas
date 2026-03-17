7b4dbcc5d7192a875ac076bd06a040de748914cd
MariaLipiz <marialipizfernandez@gmail.com>
2026-03-17 13:46:24 +0100

    nueva landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,98 +1,80 @@
-
 import Link from "next/link";
+import MailerLiteInlineForm from "../components/MailerLiteInlineForm";
 
 export default function Home() {
     return (
       <main>
       
       <section className="section  space-y-6">
-        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl lg:text-6xl">  
-        <span className="bg-[#b0e4fc]">Aprueba mates</span> sin estudiar como un desgraciado, sin ansiedad y sin llorar.
+        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl lg:text-6xl"> ¿El grado en&nbsp;
+
+        <span className="bg-[#b0e4fc]">matemáticas</span> te trae por la calle de la amargura?
         </h1>
         <h2 className="mx-auto text-gray-900 text-xl font-semibold tracking-tighter sm:text-2xl lg:text-3xl">
-          Si estás harto de estudiar como un loco, suspender y sentirte idiota, esto es para ti. Solo para 
-          estudiantes del <span className="text-decoration: underline">Grado en Matemáticas.</span>
+         Tranquilo, no es culpa tuya.
         </h2> 
       </section>
 
-      <section className="section space-y-6 text-base sm:text-xl  font-normal mt-4">
+      <section className="section space-y-6 mb-12 text-base sm:text-xl  font-normal mt-4">
         <p>
-          Se como te sientes. 
+        Tus apuntes son un cacao y suenan a chino. Aparentemente nada tiene sentido.
         </p>
 
         <p>
-        Tus apuntes te suenan a chino.
+        Definición. Proposición. Teorema. Corolario… Y así, en bucle.
         </p>
         <p>
-        El profesor viene a clase a hacer de todo menos explicar. O no viene porque no tienes profesor.
+        Mecanizar ejercicios es misión imposible. Dos ejercicios iguales se razonan de maneras distintas.
         </p>
         <p>
-        Los examenes son una ida de olla y aparecen cosas que no has visto en tu vida.
+        El profesor viene a clase a hacer de todo menos explicar y todo se da por sabido. -Esto ya se vio el año pasado-.
         </p>
         <p>
-        De la nada, te piden inventarte cosas y sacarte resultados de la manga que no sabías ni que existían.
+        O no viene porque no tienes profesor.
         </p>
         <p>
-        Echas horas y horas en la biblioteca o en casa estudiando porque ves que sino, no te da. No tienes tiempo para ti.
+        Los exámenes son una ida de olla y aparecen cosas que ni has visto ni nadie te había dicho que podían caer.
         </p>
         <p>
-        Tampoco puedes recurrir a nadie, porque tus amigos están mas perdidos que tú.
+        De la nada, te piden inventarte cosas y sacarte resultados de la manga que no sabías ni que existían. 
         </p>
         <p>
-        Y no encuentras profesor particular que te salve.
+        Echas horas y horas en la biblioteca o en casa estudiando porque ves que sino, no te da. 
         </p>
         <p>
-        En fin, que necesitas ayuda. Ayuda urgente.
+        No tienes tiempo para ti y ves que no llegas a todo. La carga de trabajo es descomunal.
         </p>
+        <p>
+        Y no encuentras paper, libro ni video de Youtube que te salve. 
+        </p>
+        <p className="mt-16">
+        Por eso, te traigo <span className="font-semibold">una estrategia que podrás aplicar hoy mismo</span> para hacer tu estudio más eficiente, y ya de paso, que todo deje de sonar a chino.
+        </p>
+        <p>
+        ¿Que hay que hacer para conseguirla?
+        </p>
+        <p>
+        Dejas <span className="font-semibold">aquí abajo</span> tu correo.
+        </p>
+        <MailerLiteInlineForm />
+
+        <h2 className="mt-12 mx-auto text-gray-900 text-xl font-semibold tracking-tighter sm:text-2xl lg:text-3xl">
+         Si te apuntas, también recibirás...
+        </h2> 
+
+        <ul className="list-disc pl-6 space-y-4 mb-10">
+          <li>Un <span className="font-semibold">consejo cada día</span> para aprobar más y hacer tu vida universitaria un poquito más llevadera</li>
+          <li>Acceso a <span className="font-semibold">productos exclusivos</span> para estudiantes de matemáticas</li>
+          <li>Una dosis de humor y motivación diaria</li>
+        </ul>
+
         <p className="mb-12">
-        Apuntate a mi método y salva el cuatri (hay lista de espera, por lo que si no te apuntas ahora, es muy probable que pierdas tu plaza).
+        Y recuerda, <span className="font-semibold">día que estás fuera, consejo que te pierdes.</span>
         </p>
-        <Link
-        href="/aprueba-mates-ya"
-        className="rounded-xl border px-4 py-2 font-medium shadow-sm disabled:opacity-60"
-      >
-        Quiero apuntarme a tu método para aprobar mates de una vez por todas
-      </Link>
-        <h2 className="mx-auto text-gray-900 text-xl font-semibold tracking-tighter sm:text-2xl lg:text-3xl mt-12">
-           <span className="bg-[#b0e4fc]">¿Quién</span> te está diciendo esto?
-        </h2>
-        <p>
-        Soy María Lípiz.
-        </p>
-        <p>
-        Ahora soy profesora, y antes también he sido estudiante del grado en matemáticas como tú.
-        </p>
-        
-        <p>
-        Por eso y porque cursé el grado a la par que trabajaba 8 horas al día, se lo que funciona y lo que no.
-        </p>
-        <p>
-        Y se como ayudarte.
-        </p>
-        <p className="mb-12">
-        Apuntate al método que ha hecho aprobar a decenas de alumnos como tú y olvidate de estudiar, no aprobar y sentirte idiota.
-        </p>
-        <Link
-        href="/aprueba-mates-ya"
-        className="rounded-xl border px-4 py-2 font-medium shadow-sm disabled:opacity-60"
-      >
-        Quiero apuntarme a tu método para aprobar mates de una vez por todas
-      </Link>
-
-      <p className="mb-12">
-        
-        </p>
-        
-        
-        
-
-      
-        
-        
+        <MailerLiteInlineForm />
   
         
       </section>
       </main>
     )
   }
-  
